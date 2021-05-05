74898035b3116898f757951888b5dba5b7a401b0
Gadjet17 <Verhi41AVUg>
2021-05-06 02:08:10 +0300

    ДЗ Урок 2, Задачи 1-5

---
+++ src/main/java/lesson2/task6/RandomMassiveInvert.java
@@ -0,0 +1,51 @@
+package lesson2.task6;
+
+import java.util.Random;
+
+public class RandomMassiveInvert {
+    public static void main(String[] args) {
+        int m = new Random().nextInt(3) + 3;
+        int n = new Random().nextInt(3) + 3;
+        int [][] firstMatrix = new int[m][n];
+
+        //заполняем матрицу
+        for (int line=0; line<m; line++){
+            for (int column=0; column<n; column++){
+                firstMatrix[line][column] = new Random().nextInt(2);
+            }
+        }
+        //заполняем случайный элемент(исключая края) двойкой
+        int randomLine = new Random().nextInt(m-2) + 1;;
+        int randomColumn = new Random().nextInt(m-2) + 1;
+        firstMatrix [randomLine][randomColumn] = 2;
+
+        int [][] secondMatrix = new int[m][n];
+        boolean isTwo = false;
+        for (int line=0; line<m; line++){
+            for (int column=0; column<n; column++){
+                    if (firstMatrix[line][column] == 2) {
+                        secondMatrix[line][column] = 2;
+                        isTwo=true;
+                    }
+                    else secondMatrix[line][column] = isTwo
+                            ? (firstMatrix[line][column] + 1) % 2
+                            : firstMatrix[line][column];
+                    }
+        }
+
+
+        System.out.println("Исходная матрица");
+        for (int[] row : firstMatrix) {
+            for (int element : row)
+                System.out.print(element + " ");
+            System.out.println();
+        }
+
+        System.out.println("Инвертированная матрица");
+        for (int[] row : secondMatrix) {
+            for (int element : row)
+                System.out.print(element + " ");
+            System.out.println();
+        }
+    }
+}
