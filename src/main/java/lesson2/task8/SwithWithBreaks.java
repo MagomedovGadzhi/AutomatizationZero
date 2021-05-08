003d79f6d50ca585b729026e912e4a88a90a4db5
Gadjet17 <Verhi41AVUg>
2021-05-08 17:33:30 +0300

    ДЗ Урок 2, Задачи 1-8

---
+++ src/main/java/lesson2/task8/SwithWithBreaks.java
@@ -0,0 +1,32 @@
+package lesson2.task8;
+
+public class SwithWithBreaks {
+    public static void main(String[] args) {
+        SwithWithBreaks example = new SwithWithBreaks();
+        example.printA(-1);
+        example.printA(2);
+        example.printA(4);
+        example.printA(7);
+        example.printA(0);
+    }
+
+    public void printA (int n) {
+        switch (n){
+            case (1):
+                System.out.println("A");
+                break;
+            case (2):
+                System.out.println("AA");
+                break;
+            case (3):
+                System.out.println("AAA");
+                break;
+            case (4):
+                System.out.println("AAAA");
+                break;
+            default:
+                System.out.println("AAAAA");
+                break;
+        }
+    }
+}
---
+++ src/main/java/lesson2/task8/SwithWithoutBreaks.java
@@ -0,0 +1,27 @@
+package lesson2.task8;
+
+public class SwithWithoutBreaks {
+    public static void main(String[] args) {
+        SwithWithBreaks example = new SwithWithBreaks();
+        example.printA(-1);
+        example.printA(2);
+        example.printA(4);
+        example.printA(7);
+        example.printA(0);
+    }
+
+    public void printA (int n) {
+        switch (n){
+            case (1):
+                System.out.println("A");
+            case (2):
+                System.out.println("AA");
+            case (3):
+                System.out.println("AAA");
+            case (4):
+                System.out.println("AAAA");
+            default:
+                System.out.println("AAAAA");
+        }
+    }
+}
