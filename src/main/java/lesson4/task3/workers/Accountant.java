0287f48c23ff73fa3b454d0f2aee8b3d488748f9
Gadjet17 <Verhi41AVUg>
2021-05-27 21:17:17 +0300

    ДЗ Урок 4, Задачи 1-3.

---
+++ src/main/java/lesson4/task3/workers/Accountant.java
@@ -0,0 +1,7 @@
+package lesson4.task3.workers;
+
+class Accountant {
+    public static void takeTicket(int ticketNumber) {
+        System.out.printf("Заявка %d была принята бухгалтером.%n", ticketNumber);
+    }
+}
